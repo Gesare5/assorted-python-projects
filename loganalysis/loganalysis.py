3a0fda467b77082b9d74e6972ad4f1b73f328725
Gesare5 <gesare.nyangweso@gmail.com>
2024-03-18 14:59:44 +0300

    Add: Loganalysis project

---
+++ loganalysis/loganalysis.py
@@ -0,0 +1,6 @@
+# May 27 11:45:40 ubuntu.local ticky: INFO: Created ticket [#1234] (username)
+
+# Jun 1 11:06:48 ubuntu.local ticky: ERROR: Connection to DB failed (username)
+    # you need finish Exercise - 3 and name html file as user_emails.html
+
+    # you need name html files as error_message.html and user_statistics.html
