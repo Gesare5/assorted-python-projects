b21fcb97a9ef3bc7ebd3db11b162279c2a9b74c5
Gesare5 <gesare.nyangweso@gmail.com>
2024-03-29 16:20:17 +0300

    Add: Sample Directory Add: Get_path_list function to average lines in a directory project

---
+++ scan_through_files/sample_directory/sample.js
@@ -0,0 +1,38 @@
+function sample() {
+  return {
+    palette: {
+      primary: {
+        main: '#ee572e',
+        lighter: '#e6e6e6',
+        hover: '#ffffff',
+      },
+      primaryDark: {
+        main: '#ee432d',
+      },
+      primaryPurple: {
+        main: '#703770',
+      },
+      secondary: {
+        main: '#22cb7f',
+        contrastText: '#eeeeee',
+        shadeText: '#555555',
+      },
+      neutral: {
+        main: '#ff3f3f'
+      },
+      accent: {
+        main: '#2c7ebf',
+      },
+      tertiary: {
+        main: '#58bc56',
+      },
+      shade: {
+        main: 'rgba(0,0,0,0.35)',
+        dark: 'rgba(0,0,0,0.75)',
+      },
+    },
+    status: {
+      danger: '#f64f4f',
+    },
+  };
+}
